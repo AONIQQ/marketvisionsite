62e945392ae1b60d0c22ba02e089895c137a3a2d
Andrewolson1 <aolson358@gmail.com>
2024-09-07 12:35:12 -1000

    added MS clarity tracking + updated metadata

--- app/websitecreation/page.tsx
+++ app/websitecreation/page.tsx
@@ -568,3 +568,8 @@ export default function WebsiteCreation() {
     </div>
   )
 }
+
+export const metadata = {
+  title: 'Custom Website Creation in 24 hours',
+  description: 'Get a fully customized, responsive website in 24 hours with Aoniqq. We use AI and human developers + copywriters to create a website that is tailored to your needs.',
+};
