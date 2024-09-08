9405059b7d259bea83371d0cbab670c6688b5ce2
Andrewolson1 <aolson358@gmail.com>
2024-09-07 20:30:47 -1000

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,21 @@ export default function RootLayout({
           }}
         />
         {/* End Microsoft Clarity Tracking Code */}
+
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16695150096"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16695150096');
+            `,
+          }}
+        />
+        {/* End Google Tag */}
+        
       </head>
       <body className={inter.className}>{children}</body>
     </html>
